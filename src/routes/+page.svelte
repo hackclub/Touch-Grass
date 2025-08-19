0ee845ebe52280ead5ade24446d9aaf0ed35bdb0
Gus Ruben <gusruben@protonmail.com>
2025-08-19 15:33:29 -0400

    WIP
    Co-authored-by: Alex Van Doren <vandorena@users.noreply.github.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -53,21 +53,38 @@
 	});
 </script>
 
-<div class="bg-sky-1 w-full min-h-screen relative">
+<div class="bg-sky-1 w-full min-h-screen relative flex flex-col">
 	<div
-	class="bg-[url(/clouds1.png)] bg-size-[100%] bg-bottom bg-no-repeat absolute h-screen top-0 w-full left-0"
-	id="clouds-1"
+		class="bg-[url(/clouds1.png)] bg-cover bg-bottom bg-no-repeat absolute h-screen top-0 w-full left-0"
+		id="clouds-1"
 	></div>
 	<div
-		class="bg-[url(/stars.png)] bg-size-[100%] bg-bottom bg-no-repeat absolute h-screen top-0 w-full left-0"
+		class="bg-[url(/stars.png)] bg-cover bg-top bg-repeat-y absolute h-screen top-0 w-full left-0"
 		id="stars"
 	></div>
 	<div
-		class="bg-[url(/clouds2.png)] bg-size-[100%] bg-bottom bg-no-repeat absolute h-screen top w-full left-0"
+		class="bg-[url(/clouds2.png)] bg-cover bg-bottom bg-no-repeat absolute h-screen top w-full left-0"
 		id="clouds-2"
 	></div>
+
+	<div class="w-full grow-1"></div>
+
 	<!-- bottom content -->
-	<div class="w-full relative">
-		
+	<div class="relative w-full flex flex-col">
+		<div
+			class="transition-transform duration-300 ease-in-out hover:translate-y-10 justify-center items-center flex flex-col"
+		>
+			<a href="https://submit.hackclub.com/touch-grass">
+				<img
+					src="/hand.png"
+					alt="Submit to Touch Grass"
+					class="block"
+				/>
+			</a>
+		</div>
+		<div class="relative min-h-64">
+			<div class="absolute bottom-0 left-0 bg-[url(/grass1.png)] w-full min-h-64 min-w-128 bg-contain bg-bottom-left bg-no-repeat"></div>
+			<div class="absolute bottom-0 right-0 bg-[url(/grass2.png)] w-full min-h-64 min-w-128 bg-contain bg-bottom-right bg-no-repeat"></div>
+		</div>
 	</div>
 </div>
